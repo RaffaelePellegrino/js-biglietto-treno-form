b934324efbb810de93e6caa3e3ec6635ee7ffaec
RaffaelePellegrino <raffaele.pellegrino02@gmail.com>
2024-11-08 18:33:53 +0100

    prima parte grafica

--- script.js
+++ script.js
@@ -17,25 +17,49 @@ function ticket(){
 
     const km = document.getElementById("km-input").value;
 
+    const telefono = document.getElementById("telefono").value;
+
     const check = document.getElementById("invalidCheck").checked;
+
+    let fasciaetà = ""
     if (!nome || !cognome || !email || !km || !check){
         alert("Inserire tutti i dati");
         return;
     }
     let prezzoTotale = parseInt(km) * prezzoKm;
     
-    if (età === 1){
+    if (età === "0-14"){
         prezzoTotale *= 0.5;
-    }else if (età === 2){
+    }else if (età === "14-35"){
         prezzoTotale *= 0.8;
     }
     console.log(nome)
     bigliettoGenerato.innerHTML = `
-        <h2>Dettagli Ticket</h2>
-        <p>${nome}</p>
-        <p>${cognome}</p>
-        <p>${email}</p>
-        <p>${prezzoTotale.toFixed(2)}$</p>`
+        <h1 class="white mb-3 mt-3">Dettagli Ticket</h1>
+        <div class="row mb-3">
+            <div class="col-4 mb-3">
+                <p>Nome: ${nome}</p>
+            </div>
+            <div class="col-4 mb-3">
+                <p>Cognome: ${cognome}</p>
+            </div>
+            <div class="col-4 mb-3">
+                <p>Età: ${età}</p>
+            </div>
+            <div class="col-5 mb-3">
+                <p>Email: ${email}</p>
+            </div>
+            <div class="col-5 mb-3">
+                <p>Recapito telefonico: ${telefono}</p>
+            </div>
+            <div class="col-2 mb-3">
+                <p>Costo biglietto: ${prezzoTotale.toFixed(2)}$</p>
+            </div>
+            <div class="col-12 mb-3 ">
+                <a href="javascript:history.go(0)" class="btn btn-light" role="button">Torna sulla pagina principale!</a>
+            </div>
+        </div>
+        `
 }
 
 bottone.addEventListener("click", function(event){
